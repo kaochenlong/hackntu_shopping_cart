939a1fba048b606c3cd97e27d55bb36b27cffcca
Eddie Kao <eddie@digik.com.tw>
2016-12-12 21:32:38 +0800

    add test 4 for cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,29 @@
 class Cart
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(product_id)
+    # 檢查是否有重複
+    item = items.find { |i| i.product_id == product_id }
+
+    if item
+      # 有的話，數量加 1
+      item.increment
+    else
+      # 沒有的話，加入 items
+      @items << CartItem.new(product_id)
+    end
+  end
+
+  def empty?
+    items.empty?
+  end
+
+  def total_price
+    items.reduce(0) { | sum, item| sum + item.total_price }
+  end
 end
 
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,54 +1,11 @@
 require 'rails_helper'
 
-class Cart
-  attr_reader :items
-
-  def initialize
-    @items = []
-  end
-
-  def add_item(product_id)
-    # 檢查是否有重複
-    item = items.find { |i| i.product_id == product_id }
-
-    if item
-      # 有的話，數量加 1
-      item.increment
-    else
-      # 沒有的話，加入 items
-      @items << CartItem.new(product_id)
-    end
-  end
-
-  def empty?
-    items.empty?
-  end
-end
-
-class CartItem
-  attr_reader :product_id, :quantity
-
-  def initialize(product_id, quantity = 1)
-    @product_id = product_id
-    @quantity = quantity
-  end
-
-  def increment(n = 1)
-    @quantity += n
-  end
-
-  def product
-    Product.find_by(id: @product_id)
-  end
-end
-
 RSpec.describe Cart, type: :model do
   describe "購物車基本功能" do
 
     it "可以新增商品到購物車裡，然後購物車裡就有東西了" do
       cart = Cart.new
       cart.add_item(1)
-      #expect(cart.empty?).to be false
       expect(cart).not_to be_empty
       expect(cart.items.count).to be 1
     end
@@ -74,7 +31,18 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.first.product).to be_a Product
     end
 
-    #* 可以計算整個購物車的總消費金額
+    it "可以計算整個購物車的總消費金額" do
+      p1 = Product.create(title:'aaa', price:100)
+      p2 = Product.create(title:'aaa', price:50)
+
+      cart = Cart.new
+      3.times { cart.add_item(p1.id) }
+      5.times { cart.add_item(p2.id) }
+
+      expect(cart.items.first.total_price).to be 300
+      expect(cart.items.last.total_price).to be 250
+      expect(cart.total_price).to be 550
+    end
   end
 
   describe "購物車進階功能" do
