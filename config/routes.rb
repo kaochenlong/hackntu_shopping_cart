39ac14726a4b188440d458324fe1a577eb4e5d9c
Eddie Kao <eddie@digik.com.tw>
2016-12-19 19:39:13 +0800

    add routes for cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :cart, only:[:show, :destroy] do
+    member do
+      post :add, path: 'add/:id'
+    end
+
+    collection do
+      get :checkout
+    end
+  end
 end
