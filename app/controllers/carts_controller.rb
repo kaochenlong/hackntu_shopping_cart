b78fba440584f8577ff73ebfb04c09ab47bd4d6a
Eddie Kao <eddie@digik.com.tw>
2016-12-19 20:11:01 +0800

    add cart show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,6 +8,9 @@ class CartsController < ApplicationController
     redirect_to products_path, notice: '已放至購物車'
   end
 
+  def show
+  end
+
   private
   def find_product
     @product = Product.find_by(id: params[:id])
