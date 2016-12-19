1961275acfe91781e7a08edc2ee3aeca7832ec8e
Eddie Kao <eddie@digik.com.tw>
2016-12-19 19:19:51 +0800

    add product scaffold

--- db/migrate/20161219111821_create_products.rb
+++ db/migrate/20161219111821_create_products.rb
@@ -3,6 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
     create_table :products do |t|
       t.string :title
       t.integer :price
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212131922) do
+ActiveRecord::Schema.define(version: 20161219111821) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
